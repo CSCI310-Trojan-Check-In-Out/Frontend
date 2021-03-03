729c6a8a8395209822d0cfc840351475e44389a1
Shihao Huang <huan773@usc.edu>
2021-03-03 15:49:53 -0800

    update

--- src/screens/tabNavigatorScreens/Profile.tsx
+++ src/screens/tabNavigatorScreens/Profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Text} from 'react-native';
-
+import IconButton from "../../components/IconButton"
 export default function Profile() {
   return <Text>Profile</Text>;
 }
