94aa2a8620ce364800779615cad0f0ff00ee7065
Shihao Huang <huan773@usc.edu>
2021-03-03 23:11:42 -0800

    update

--- src/screens/tabNavigatorScreens/Profile.tsx
+++ src/screens/tabNavigatorScreens/Profile.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import IconButton from "../../components/IconButton"
+import IconButton from '../../components/IconButton';
 export default function Profile() {
-  return <>
-  <Text>Profile</Text>
-  <IconButton iconName='facebook' text='Log out'></IconButton>
-  </>
+  return (
+    <>
+      <Text>Profile</Text>
+      <IconButton iconName={'facebook'} text={'Log out'} />
+    </>
+  );
 }
