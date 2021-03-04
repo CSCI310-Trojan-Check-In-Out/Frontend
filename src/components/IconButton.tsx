43c5f99636e1b6ad665119fec8e6ca4af56e076a
Shihao Huang <huan773@usc.edu>
2021-03-04 00:03:05 -0800

    update

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import {StyleSheet,View, Text, Icon, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, Text, Icon, TouchableOpacity} from 'react-native';
 export default function IconButton({iconName, text}) {
-
-  return <>
-  <TouchableOpacity>
-  <View>
-  <Icon
-    name={iconName}
-    style={styles.icon}/>
-  <Text style={styles.text}>{text}</Text>
-</View>
-</TouchableOpacity>
-</>;
+  return (
+    <>
+      <TouchableOpacity>
+        <View>
+          <Icon name={iconName} style={styles.icon} />
+          <Text style={styles.text}>{text}</Text>
+        </View>
+      </TouchableOpacity>
+    </>
+  );
 }
 
-const styles = StyleSheet({
-  icon:{
+const styles = StyleSheet.create({
+  icon: {
     height: 25,
     width: 25,
   },
-  text:{
+  text: {
     fontSize: 18,
     color: '#FAFAFA',
     marginLeft: 10,
     marginTop: 2,
-  }
+  },
 });
