85fcada1564a6e119eb2ca0403eac52b53fc7b4a
Shihao Huang <huan773@usc.edu>
2021-03-05 18:00:40 -0800

    update

--- src/screens/outerMostStackScreens/SignUp.tsx
+++ src/screens/outerMostStackScreens/SignUp.tsx
@@ -14,6 +14,7 @@ import {useSignUp} from '../../hooks/useSignUp';
 import CommonStyle from '../../style/common.style';
 import Theme from '../../style/theme.style';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export default function SignUp({navigation}: {navigation: any}) {
   const [
@@ -31,107 +32,118 @@ export default function SignUp({navigation}: {navigation: any}) {
 
   return (
     <>
-      <View style={CommonStyle.outerContainerStyle}>
-        {/* title */}
-        <Text style={CommonStyle.title}>Connect Your USC Account</Text>
-        <View style={CommonStyle.locationBoxContainer}>
-          <Text>Profile Pic</Text>
-        </View>
-
-        {/* inputs */}
-        <View style={{width: Theme.CONTAINER_WIDTH_LARGE}}>
-          <TextInput
-            style={CommonStyle.inputBoxStyle}
-            onChangeText={setFullName}
-            value={fullName}
-            placeholder={'FULL NAME'}
-          />
-          <TextInput
-            style={CommonStyle.inputBoxStyle}
-            onChangeText={setUscID}
-            value={uscID}
-            placeholder={'USC ID'}
-          />
-          <TextInput
-            style={CommonStyle.inputBoxStyle}
-            onChangeText={setEmail}
-            value={email}
-            placeholder={'USC EMAIL'}
-          />
-          <TextInput
-            style={CommonStyle.inputBoxStyle}
-            onChangeText={setMajor}
-            value={major}
-            placeholder={'MAJOR'}
-          />
-        </View>
-
-        <View
-          style={{
-            marginTop: 15,
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            flexDirection: 'row',
-            width: '40%',
-          }}>
-          <Text style={{fontSize: Theme.FONT_SIZE_MEDIUM}}>I am a: </Text>
+      <ScrollView style={{flex:1, height:"100%"}}>
+        <View style={CommonStyle.outerContainerStyle}>
+          {/* title */}
+          <Text style={CommonStyle.title}>Connect Your USC Account</Text>
           <View
             style={{
+              borderRadius: 50,
               display: 'flex',
-              height: 60,
-              justifyContent: 'space-around',
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: 'white',
+              height: 300,
+              width: '80%',
             }}>
-            <View>
-              <TouchableOpacity
-                onPress={() => setIsStudent(true)}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                }}>
-                <Ionicons
-                  name={
-                    !isStudent ? 'radio-button-off-sharp' : 'radio-button-on'
-                  }
-                  size={17}></Ionicons>
-                <Text style={{marginLeft: 10}}>STUDENT</Text>
-              </TouchableOpacity>
-            </View>
+            <Text>Profile Pic</Text>
+          </View>
 
-            <View>
-              <TouchableOpacity
-                onPress={() => setIsStudent(false)}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                }}>
-                <Ionicons
-                  name={
-                    isStudent ? 'radio-button-off-sharp' : 'radio-button-on'
-                  }
-                  size={17}></Ionicons>
-                <Text style={{marginLeft: 10}}>MANAGER</Text>
-              </TouchableOpacity>
+          {/* inputs */}
+          <View style={{width: Theme.CONTAINER_WIDTH_LARGE}}>
+            <TextInput
+              style={CommonStyle.inputBoxStyle}
+              onChangeText={setFullName}
+              value={fullName}
+              placeholder={'FULL NAME'}
+            />
+            <TextInput
+              style={CommonStyle.inputBoxStyle}
+              onChangeText={setUscID}
+              value={uscID}
+              placeholder={'USC ID'}
+            />
+            <TextInput
+              style={CommonStyle.inputBoxStyle}
+              onChangeText={setEmail}
+              value={email}
+              placeholder={'USC EMAIL'}
+            />
+            <TextInput
+              style={CommonStyle.inputBoxStyle}
+              onChangeText={setMajor}
+              value={major}
+              placeholder={'MAJOR'}
+            />
+          </View>
+
+          <View
+            style={{
+              marginTop: 15,
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              flexDirection: 'row',
+              width: '40%',
+            }}>
+            <Text style={{fontSize: Theme.FONT_SIZE_MEDIUM}}>I am a: </Text>
+            <View
+              style={{
+                display: 'flex',
+                height: 60,
+                justifyContent: 'space-around',
+              }}>
+              <View>
+                <TouchableOpacity
+                  onPress={() => setIsStudent(true)}
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}>
+                  <Ionicons
+                    name={
+                      !isStudent ? 'radio-button-off-sharp' : 'radio-button-on'
+                    }
+                    size={17}></Ionicons>
+                  <Text style={{marginLeft: 10}}>STUDENT</Text>
+                </TouchableOpacity>
+              </View>
+
+              <View>
+                <TouchableOpacity
+                  onPress={() => setIsStudent(false)}
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}>
+                  <Ionicons
+                    name={
+                      isStudent ? 'radio-button-off-sharp' : 'radio-button-on'
+                    }
+                    size={17}></Ionicons>
+                  <Text style={{marginLeft: 10}}>MANAGER</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
+          <View
+            style={{
+              width: Theme.CONTAINER_WIDTH_MEDIUM,
+              height: '10%',
+              justifyContent: 'space-evenly',
+            }}>
+            <Button
+              title={'Done'}
+              color={Theme.RED_PRIMARY}
+              onPress={() => {
+                navigation.navigate('TabNavigator');
+              }}
+            />
+          </View>
         </View>
-        <View
-          style={{
-            width: Theme.CONTAINER_WIDTH_MEDIUM,
-            height: '10%',
-            justifyContent: 'space-evenly',
-          }}>
-          <Button
-            title={'Done'}
-            color={Theme.RED_PRIMARY}
-            onPress={() => {
-              navigation.navigate('TabNavigator');
-            }}
-          />
-        </View>
-      </View>
+      </ScrollView>
     </>
   );
 }
--- src/style/common.style.tsx
+++ src/style/common.style.tsx
@@ -24,7 +24,7 @@ export default StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    height: '100%',
+    // height: '100%',
     width: '100%',
     alignSelf: 'center',
     flex: 1,
