d8361db8853c8afac5e217775fb15be9dd66ce11
Henry He <hejiayan@usc.edu>
2021-04-24 00:12:52 -0700

    update addLocation UI

--- src/screens/locationSearch/AddLocation.tsx
+++ src/screens/locationSearch/AddLocation.tsx
@@ -257,7 +257,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'center',
-    color:'grey',
+    color:'#949494',
     width: '100%',
   },
   buttonContainer: {
